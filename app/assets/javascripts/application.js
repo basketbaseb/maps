2d552299c6b8f4127867466ef80a494e6825ba19
Nathan Connor <natyconnor@gmail.com>
2015-04-09 14:58:23 -0700

    If Android or iOS, pano div uses up full width

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,13 +18,10 @@
 
 function detectBrowser() {
   var useragent = navigator.userAgent;
-  var mapdiv = document.getElementById("map-canvas");
+  var mapdiv = document.getElementById("pano");
 
   if (useragent.indexOf('iPhone') != -1 || useragent.indexOf('Android') != -1 ) {
     mapdiv.style.width = '100%';
     mapdiv.style.height = '100%';
-  } else {
-    mapdiv.style.width = '600px';
-    mapdiv.style.height = '800px';
   }
 }
