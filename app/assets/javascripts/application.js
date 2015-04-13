3157d9ce5fc9cf8de8ccf7eea85311741e534da7
Nathan Connor <natyconnor@gmail.com>
2015-04-13 15:50:38 -0700

    Added right-click menu for deleting markers.
    Also added google-maps to asset pipeline so functions can be placed in any files in the pipeline.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -25,3 +25,81 @@ function detectBrowser() {
     mapdiv.style.height = '100%';
   }
 }
+
+
+/**
+ * A menu that lets a user delete a selected marker
+ * @constructor
+ */
+function DeleteMenu() {
+  this.div_ = document.createElement('div');
+  this.div_.className = 'delete-menu';
+  this.div_.innerHTML = 'Delete';
+
+  var menu = this;
+  google.maps.event.addDomListener(this.div_, 'click', function() {
+    menu.removeMarker();
+  });
+}
+
+DeleteMenu.prototype = new google.maps.OverlayView();
+
+DeleteMenu.prototype.onAdd = function() {
+  deleteMenu = this;
+  var map = this.getMap();
+  this.getPanes().floatPane.appendChild(this.div_);
+
+  // mousedown anywhere on the map except on the menu div will close the
+  // menu.
+  this.divListener_ = google.maps.event.addDomListener(map.getDiv(), 'mousedown', function(e) {
+    if (e.target != deleteMenu.div_) {
+      deleteMenu.close();
+    }
+  }, true);
+};
+
+DeleteMenu.prototype.onRemove = function() {
+  google.maps.event.removeListener(this.divListener_);
+  this.div_.parentNode.removeChild(this.div_);
+
+  // clean up
+  this.set('position');
+  this.set('marker');
+};
+
+DeleteMenu.prototype.close = function() {
+  this.setMap(null);
+};
+
+DeleteMenu.prototype.draw = function() {
+  var position = this.get('position');
+  var projection = this.getProjection();
+
+  if (!position || !projection) {
+    return;
+  }
+
+  var point = projection.fromLatLngToDivPixel(position);
+  this.div_.style.top = point.y + 'px';
+  this.div_.style.left = point.x + 'px';
+};
+
+/**
+ * Opens the menu at a Marker
+ */
+DeleteMenu.prototype.open = function(map, marker) {
+  this.set('position', marker.position);
+  this.set('marker', marker);
+  this.setMap(map);
+  this.draw();
+};
+
+/**
+ * Deletes the marker
+ */
+DeleteMenu.prototype.removeMarker = function() {
+  var marker = this.get('marker');
+  marker.setMap(null);
+  this.close();
+};
+
