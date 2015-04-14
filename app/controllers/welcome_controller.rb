01f70ab7a1ca441de9c68ef47cff5279753afd95
David Wosk <dwosk@Davids-MacBook-Pro-3.local>
2015-04-14 16:58:30 -0700

    logs to console only 5 businesses not 15

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,7 @@ class WelcomeController < ApplicationController
   def index
   end
   def search
-	parameters = { term: params[:term], limit: 16 }
+	parameters = { term: params[:term], limit: 5 }
 	render json: Yelp.client.search('San Francisco', parameters)
   end
 end
