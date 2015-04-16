66731821aaf5cf5f2c2c42ee2922510549ac11a4
David Wosk <dwosk@Davids-MacBook-Pro-3.local>
2015-04-16 16:02:06 -0700

    business api returns review of the business.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,10 +1,17 @@
 class WelcomeController < ApplicationController
   def index
   end
+
   def search
 	parameters = { term: params[:term], limit: 5, sort: 2, radius_filter: 8046 }
 	coordinates = params[:coordinates]
 	# render json: Yelp.client.search(params[:location], parameters)
 	render json: Yelp.client.search_by_coordinates(coordinates, parameters)
   end
+
+  def business
+  	parameters = params[:id]
+	render json: Yelp.client.business(parameters)
+  end
+
 end
